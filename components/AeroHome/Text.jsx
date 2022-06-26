21d2ebee92234498b896efd2ed4d3b80d891a156
Stephengade <gbolabyte222@gmail.com>
2022-06-26 15:18:45 +0100

    landing page done

---
+++ components/AeroHome/Text.jsx
@@ -0,0 +1,23 @@
+ 
+ 
+ <><div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    <Image src={Mockup} className="w-full" />
+  </div>
+  <div id="item2" className="carousel-item w-full">
+    <Image src={Mockup} className="w-full" />
+  </div>
+  <div id="item3" className="carousel-item w-full">
+    <Image src={Mockup} className="w-full" />
+  </div>
+</div><div className="flex justify-center w-full py-2 gap-2">
+    <a href="#item1" className="btn btn-xs">
+      1
+    </a>
+    <a href="#item2" className="btn btn-xs">
+      2
+    </a>
+    <a href="#item3" className="btn btn-xs">
+      3
+    </a>
+  </div></>
